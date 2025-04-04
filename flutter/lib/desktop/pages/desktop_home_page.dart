92c3ae451a84a6030d6dcee765a861bc75cde309
Pofig1sT161 <103854962+Pofig1sT161@users.noreply.github.com>
2025-04-04 11:15:52 +0300

    Update desktop_home_page.dart

--- flutter/lib/desktop/pages/desktop_home_page.dart
+++ flutter/lib/desktop/pages/desktop_home_page.dart
@@ -884,7 +884,7 @@ void setPasswordDialog({VoidCallback? notEmptyCallback}) async {
   final RxString rxPass = pw.trim().obs;
   final rules = [
     DigitValidationRule(),
-    UppercaseValidationRule(),
+    //UppercaseValidationRule(),
     LowercaseValidationRule(),
     // SpecialCharacterValidationRule(),
     MinCharactersValidationRule(8),
